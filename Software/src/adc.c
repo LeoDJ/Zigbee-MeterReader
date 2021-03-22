3fc3ba28dda14e343fba8763abc3866ed5e10d00
LeoDJ <leandro.spaeth@gmail.com>
2021-03-22 02:55:26 +0100

    restructure ADC before adding phototransistor input

--- Software/src/adc.c
+++ Software/src/adc.c
@@ -1,64 +1,77 @@
 #include "adc.h"
 #include <nrf_log.h>
+#include <nrfx_saadc.h>
 
 
 // set input to VDDH and divide by 10
-nrf_saadc_channel_config_t adc_vddh_cfg = {
+nrf_saadc_channel_config_t adcChCfg_vddh = {
     .resistor_p = NRF_SAADC_RESISTOR_DISABLED,
     .resistor_n = NRF_SAADC_RESISTOR_DISABLED,
     .gain       = NRF_SAADC_GAIN1_2,
     .reference  = NRF_SAADC_REFERENCE_INTERNAL,
-    .acq_time   = NRF_SAADC_ACQTIME_10US,
+    .acq_time   = NRF_SAADC_ACQTIME_40US,
     .mode       = NRF_SAADC_MODE_SINGLE_ENDED,
     .burst      = NRF_SAADC_BURST_ENABLED,
     .pin_p      = SAADC_CH_PSELP_PSELP_VDDHDIV5,
     .pin_n      = NRF_SAADC_INPUT_DISABLED,
 };
 
+// nrfx_saadc_config_t adcCfg_vddh = {
+//     .low_power_mode = true,
+//     .resolution = NRF_SAADC_RESOLUTION_14BIT,
+//     .oversample = NRF_SAADC_OVERSAMPLE_8X,
+//     .interrupt_priority = APP_IRQ_PRIORITY_LOW
+// };
+
+nrf_saadc_channel_config_t adcChCfg_sensor = {
+    .resistor_p = NRF_SAADC_RESISTOR_DISABLED,
+    .resistor_n = NRF_SAADC_RESISTOR_DISABLED,
+    .gain       = NRF_SAADC_GAIN1_6,
+    .reference  = NRF_SAADC_REFERENCE_INTERNAL,
+    .acq_time   = NRF_SAADC_ACQTIME_10US,
+    .mode       = NRF_SAADC_MODE_SINGLE_ENDED,
+    .burst      = NRF_SAADC_BURST_ENABLED,
+    .pin_p      = SAADC_CH_PSELP_PSELP_AnalogInput0,
+    .pin_n      = NRF_SAADC_INPUT_DISABLED,
+};
+
+nrfx_saadc_config_t adcCfg_sensor = {
+    .low_power_mode = true,
+    .resolution = NRF_SAADC_RESOLUTION_12BIT,
+    .oversample = NRF_SAADC_OVERSAMPLE_DISABLED,
+    .interrupt_priority = APP_IRQ_PRIORITY_LOW
+};
+
 static nrf_saadc_value_t adcBuf[2][ADC_SAMPLE_NUM];
 
 static nrf_saadc_value_t convertedSamples[1];
 
-void adcCallback(nrf_drv_saadc_evt_t const * p_event) {
-    if (p_event->type == NRF_DRV_SAADC_EVT_DONE) {
-        nrf_drv_saadc_buffer_convert(p_event->data.done.p_buffer, 1);
+void adcCallback(nrfx_saadc_evt_t const * p_event) {
+    if (p_event->type == NRFX_SAADC_EVT_DONE) {
+        nrfx_saadc_buffer_convert(p_event->data.done.p_buffer, ADC_SAMPLE_NUM);
         convertedSamples[0] = p_event->data.done.p_buffer[0];
+        NRF_LOG_INFO("Called adc callback");
     }
     
 }
 
-void adcDoSample() {
-    nrf_drv_saadc_sample();
-}
-
-bool adcBlockUntilConversionComplete() {
-    uint32_t timeout = 10000; // just guessed
-    while (nrf_saadc_event_check(NRF_SAADC_EVENT_END) && timeout > 0) {
-        timeout--;
-    }
-    return !!timeout; // true if successful and no timeout occured
-}
-
 uint32_t adcGetVcc() {
-    adcDoSample();
-    adcBlockUntilConversionComplete(); // just assume this works without a problem for now
-    nrf_saadc_value_t adcVal = convertedSamples[0];
+    nrfx_saadc_channel_init(1, &adcChCfg_vddh);
+
+    nrf_saadc_value_t adcVal;
+    nrfx_saadc_sample_convert(1, &adcVal); // blocking, TODO: check if other conversion is running
     uint32_t batVolt = ((uint32_t)adcVal) * 1000 / ADC_COUNTS_PER_VOLT;
     NRF_LOG_INFO("ADC: %d -> %d mV", adcVal, batVolt);
+
+    nrfx_saadc_channel_uninit(1);
+
     return batVolt;
 }
 
 void adcInit() {
-
-    nrf_drv_saadc_config_t saadc_config = {
-        .low_power_mode = true,
-        .resolution = NRF_SAADC_RESOLUTION_14BIT,
-        .oversample = NRF_SAADC_OVERSAMPLE_8X,
-        .interrupt_priority = APP_IRQ_PRIORITY_LOW
-    };
-
-    nrf_drv_saadc_init(&saadc_config, adcCallback);
-    nrf_drv_saadc_channel_init(0, &adc_vddh_cfg);
-    nrf_drv_saadc_buffer_convert(adcBuf[0], ADC_SAMPLE_NUM);
-    nrf_drv_saadc_buffer_convert(adcBuf[1], ADC_SAMPLE_NUM);
+    nrfx_saadc_init(&adcCfg_sensor, adcCallback);
+    nrfx_saadc_channel_init(0, &adcChCfg_sensor);
+    nrfx_saadc_channel_init(1, &adcChCfg_vddh);
+    // nrfx_saadc_buffer_convert(adcBuf[0], ADC_SAMPLE_NUM); // currently messes with vcc sampling, TODO: find out why
+    // nrfx_saadc_buffer_convert(adcBuf[1], ADC_SAMPLE_NUM);
 }
--- Software/src/adc.h
+++ Software/src/adc.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <nrf_drv_saadc.h>
+#include <stdint.h>
 
-#define ADC_SAMPLE_NUM  1 
-// VDDH/5 / 2 => 0.6V * 10 = 6V full range -> 16383 / 6V = 2730.66
-#define ADC_COUNTS_PER_VOLT 2731
+#define ADC_SAMPLE_NUM  1 // number of active channels
+
+// VDDH/5 / 2 => 0.6V * 10 = 6V full range -> 16383 / 6V = 2730.66 | 4095 / 6V = 682.5
+#define ADC_COUNTS_PER_VOLT 682
 
 void adcInit();
-void adcDoSample();
 uint32_t adcGetVcc(); // returns mV, blocking
